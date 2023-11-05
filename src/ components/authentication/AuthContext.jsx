d6c057d6afa514ba456f60f72b1eb29ed074e437
Mark Vu <markvu2003@gmail.com>
2023-11-05 07:53:43 -0800

    fixed

--- src/ components/authentication/AuthContext.jsx
+++ src/ components/authentication/AuthContext.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { createContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import api from './api';
+import api from './Api.jsx';
 const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
