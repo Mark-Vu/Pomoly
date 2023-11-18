17a16a1178892e19ad8efa97af2276b55ce26d50
Margaret Le <margaretle1128@gmail.com>
2023-11-18 00:38:14 -0800

    divide settings to two tabs

--- client/src/ components/home_page/Settings.jsx
+++ client/src/ components/home_page/Settings.jsx
@@ -1,35 +1,45 @@
 import BackgroundOptions from './BackgroundOptions';
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
 import AuthContext from "../authentication/AuthContext";
 
-function Header() {
-    const { logout, user } = useContext(AuthContext);
+function Logout() {
+  const { logout, user } = useContext(AuthContext);
   
-  
-    function handleLogout() {
-      try {
-        logout();
-      } catch (error) {
-        console.error("Logout error:", error);
-      }
+  function handleLogout() {
+    try {
+      logout();
+    } catch (error) {
+      console.error("Logout error:", error);
     }
-    return (
-      <div>
-          <button onClick={handleLogout}>
-              Logout
-          </button>
-          <div>{user.name}</div>
-      </div>
-      
-    );
+  }
+
+  return (
+    <div>
+      <button onClick={handleLogout}>
+        Logout
+      </button>
+      <div>{user.name}</div>
+    </div> 
+  );
 }
 
 function Settings({ onClose }) {
-    return (
-        <div>
-            <Header />
-        </div>
-    );
-  }
-  
-  export default Settings;
+  const [activeTab, setActiveTab] = useState('logout');
+
+  return (
+    <div>
+      <div className="tabs">
+        <button onClick={() => setActiveTab('logout')} className={activeTab === 'logout' ? 'active' : ''}>
+          Profile
+        </button>
+        <button onClick={() => setActiveTab('background')} className={activeTab === 'background' ? 'active' : ''}>
+          Background
+        </button>
+      </div>
+      <div className="tab-content">
+        {activeTab === 'logout' && <Logout />}
+        {activeTab === 'background' && <BackgroundOptions />}
+      </div>
+    </div>
+  );
+} export default Settings;
