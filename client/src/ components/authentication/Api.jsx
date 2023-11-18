b155a64fb7d84a26b887ac3ef90a65eff1d181d3
Margaret Le <margaretle1128@gmail.com>
2023-11-17 22:29:20 -0800

    automate url changing

--- client/src/ components/authentication/Api.jsx
+++ client/src/ components/authentication/Api.jsx
@@ -3,17 +3,11 @@ import axios from 'axios';
 
 const api = axios.create({
   withCredentials: true,
-  // DEVELOPMENT BACKEND:
-  baseURL:"http://127.0.0.1:5000",
-
-  // PRODUCTION BACKEND:
-  // baseURL:"https://studyhub-backend.vercel.app/",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
-    'X-CSRF-TOKEN':localStorage.getItem('csrf_access_token')
+    'X-CSRF-TOKEN': localStorage.getItem('csrf_access_token')
   }
 });
-
-
 
 api.interceptors.response.use(
   async (response) => {
--- client/vite.config.js
+++ client/vite.config.js
@@ -3,9 +3,14 @@ import mkcert from 'vite-plugin-mkcert'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command, mode }) => {
+  let plugins = [react()];
 
-  // IF USE DEPLOYMENT BACKEND, REPLACE THE plugins above with the one below
-  // plugins: [react(), mkcert()],
-})
+  if (mode === 'production') {
+    plugins.push(mkcert());
+  }
+
+  return {
+    plugins: plugins
+  };
+});
