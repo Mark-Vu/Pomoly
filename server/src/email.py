d7bfd940c04873ab71e624b99f88c1b480da3c01
Mark Vu <markvu2003@gmail.com>
2023-11-20 22:38:35 -0800

    added flask[async] in requirements.txt

--- server/src/email.py
+++ server/src/email.py
@@ -2,19 +2,19 @@ from threading import Thread
 from flask import current_app
 from flask_mail import Message
 from src import mail
-
+import asyncio
 
 def send_async_email(app, msg):
     with app.app_context():
         mail.send(msg)
 
 
-def send_email(subject, sender, recipients, text_body, attachments=None, sync=False, production=False):
+async def send_email(subject, sender, recipients, text_body, attachments=None, sync=False, production=False):
     if production:
         msg = Message('Pomoly: Verify your account', recipients=recipients)
         msg.body = text_body
         msg.html = f'<p>{text_body}</p>'
-        Thread(target=send_async_email, args=(current_app._get_current_object(), msg)).start()
+        mail.send(msg)
     else:
         try:
             msg = Message(subject, sender=sender, recipients=recipients)
